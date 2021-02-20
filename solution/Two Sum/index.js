5c417504180bb2bdafcf818ecde5117af59e512b
RicoLiu <2869183609@qq.com>
2021-02-20 10:52:59 +0800

    Update index.js
    Add another solution

--- solution/Two Sum/index.js
+++ solution/Two Sum/index.js
@@ -13,3 +13,18 @@ var twoSum = function(nums, target) {
     }
   }
 };
+
+
+// nums = [2, 7, 11, 15], target = 9
+function twoSum(nums, target) {
+  let m = new Map();
+  let another;
+  for (let i = 0; i < nums.length; i++) {
+    another = target - nums[i];
+    if (typeof m[another] === 'number') {
+      return [m[another], i];
+    }
+    m[nums[i]] = i;
+  }
+  return null;
+}
