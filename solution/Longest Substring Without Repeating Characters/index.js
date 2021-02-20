d27bfe9c7ac55056aad840fbc0bbe14819b669e0
RicoLiu <2869183609@qq.com>
2021-02-20 14:50:08 +0800

    Update index.js
    Add Slide Window Solution

--- solution/Longest Substring Without Repeating Characters/index.js
+++ solution/Longest Substring Without Repeating Characters/index.js
@@ -17,3 +17,30 @@ var lengthOfLongestSubstring = function(s) {
   }
   return max;
 };
+
+
+
+function lengthOfLongestSubstring(str) {
+  if (str.length === 0) {
+    return 0;
+  }
+
+  // 判断滑动窗口中是否有重复字符
+  let freq = new Array(256).fill(0);
+  let result = 0, left = 0, right = -1;
+  while (left < str.length) {
+
+    if (right + 1 < str.length && freq[str.charCodeAt(right + 1) - 'a'.charCodeAt(0)] === 0) {
+      // 增加滑动窗口右边界
+      freq[str.charCodeAt(right + 1) - 'a'.charCodeAt(0)]++;
+      right++;
+    } else {
+      // 有重复字符，增加滑动窗口左边界
+      freq[str.charCodeAt(left) - 'a'.charCodeAt(0)]--;
+      left++
+    }
+    result = Math.max(result, right - left + 1);
+  }
+
+  return result;
+ }
