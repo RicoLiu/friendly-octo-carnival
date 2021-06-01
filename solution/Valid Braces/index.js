7e5a148670452bdbaa3171d98f12beee604b2fe9
RicoLiu <2869183609@qq.com>
2021-06-01 17:23:01 +0800

    Update index.js

--- solution/Valid Braces/index.js
+++ solution/Valid Braces/index.js
@@ -5,3 +5,39 @@ function validBraces(braces) {
   }
   return braces.length === 0;
 }
+
+/*******************************/
+
+const isValidStr = (s: string): boolean => {
+  if (s === null || s.length === 0) {
+    return true;
+  }
+  if (s.length % 2 === 1) {
+    return false;
+  }
+  let arr: string[] = new Array<string>();
+  for (let i = 0; i < arr.length; i++) {
+    let char: string = s.charAt(i);
+    if (char === '(' || char === '{' || char === '[') {
+      arr.push(char);
+    } else if (char === ')') {
+      if (arr.length === 0 || arr.slice(-1)[0] !== '(') {
+        return false;
+      }
+      arr.pop();
+    } else if (char === '}') {
+      if (arr.length === 0 || arr.slice(-1)[0] !== '{') {
+        return false;
+      }
+      arr.pop();
+    } else if (char === ']') {
+      if (arr.length === 0 || arr.slice(-1)[0] !== '[') {
+        return false;
+      }
+      arr.pop();
+    } else {
+      return false;
+    }
+  }
+  return arr.length === 0;
+}
